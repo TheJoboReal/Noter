d0035044eab9f5f9037c449a1c436af76b9880ca
TheJoboReal <kasper.jobo@gmail.com>
2026-01-23 16:55:14 +0100

    vault backup: 2026-01-23 16:55:14

--- Templates/DailyNotesTemp.md
+++ Templates/DailyNotesTemp.md
@@ -1,5 +1,13 @@
+---
+---
 
-## Get Done Today:
-<% tp.user.carryover_todos() %>
-
-#review
+````col
+```col-md
+> [!warning] + Overdue
+> ```tasks
+> not done
+> description includes #work
+> sort by due date
+> due before 12-12-2026
+```
+```
--- Templates/obs_todo_migr.py
+++ Templates/obs_todo_migr.py
@@ -12,21 +12,19 @@ daily_notes_dir = sys.argv[1]
 unchecked = r"- \[ \]"
 prevnote = []
 open_todos = []
-carryover_tally = "."  # append each time todo is migrated
+carryover_tally = "." # append each time todo is migrated
 
 files = [f for f in listdir(daily_notes_dir) if isfile(join(daily_notes_dir, f))]
 
 if len(files) >= 2:
     files.sort(reverse=True)
-    prevfile = files[
-        1
-    ]  # need second newest file since new note exists when this is called
+    prevfile = files[1] # need second newest file since new note exists when this is called
 
     # avoid race by waiting for the obs_dates_nav script which may be invoked at same time
     sleep(2)
 
     # grab unchecked todos to move to new day, remove from previous day's note
-    with open(daily_notes_dir + "/" + prevfile, "r+") as f:
+    with open(daily_notes_dir+"/"+prevfile, 'r+') as f:
         lines = f.readlines()
         for line in lines:
             if match(unchecked, line):
@@ -42,4 +40,4 @@ if len(files) >= 2:
     for todo in open_todos:
         print(todo[:-1] + carryover_tally)
 else:
-    print("- [ ]")
+    print('- [ ]')
